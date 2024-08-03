d8f0e90c885af151d37cebad47f66798181c8228
Gragon <gragonator02@gmail.com>
2024-08-03 20:33:18 +0200

    Added theme change poc

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+<body id="topBody" :class="[baseTheme, accentTheme]">
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</body>
 </template>
+
+<script lang="ts" setup>
+const baseTheme = useState('baseTheme', () => 'baseLight')
+const accentTheme = useState('accentTheme', () => 'accentPurple')
+
+// TODO Implement properly with more themes
+const changeAccent = () => accentTheme.value = accentTheme.value === 'accentPurple' ? 'accentLightBlue' : 'accentPurple'
+const changeBase = () => baseTheme.value = baseTheme.value === 'baseDark' ? 'baseLight' : 'baseDark'
+</script>
