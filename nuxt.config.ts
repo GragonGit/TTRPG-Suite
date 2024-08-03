bd53f9cb19023d6ab4dbe17d7db6f28369a0297d
Gragon <gragonator02@gmail.com>
2024-08-04 00:29:45 +0200

    Added supabase module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,9 +11,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "@nuxtjs/i18n",
+    "@nuxtjs/supabase",
     '@vueuse/nuxt',
   ],
   i18n: {
     vueI18n: './assets/i18n.config.ts'
+  },
+  supabase: {
+    url: 'https://rgmkkhvxzgnjztovybrd.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnbWtraHZ4emduanp0b3Z5YnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI3MjIxOTMsImV4cCI6MjAzODI5ODE5M30.H_N7pYlGk_uhxSa6LKqqRjVSLNUy0wwUqHQz7QDGJhg'
   }
 })
