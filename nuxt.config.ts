1470bce37587641254e6553894848595ddcc3846
Gragon <gragonator02@gmail.com>
2024-08-03 14:41:17 +0200

    Added i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,12 @@ export default defineNuxtConfig({
       title: "TTRPG Suite"
     }
   },
+
   compatibilityDate: '2024-08-02',
   css: ["~/assets/styles.sass"],
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    vueI18n: './assets/i18n.config.ts'
+  }
 })
