50311b52ad6a811385465ef17af6ac8809c78c76
Gragon <gragonator02@gmail.com>
2024-08-04 13:45:02 +0200

    Added username data on signup

--- pages/auth/signup.vue
+++ pages/auth/signup.vue
@@ -5,6 +5,7 @@
     <div>
       <input type="email" placeholder="Email" v-model="email" />
       <input type="password" placeholder="Password" v-model="password" />
+      <input type="text" placeholder="Username" v-model="username" />
     </div>
     <div>
       <input type="submit" :value="loading ? 'Loading' : 'Send magic link'" :disabled="loading" />
@@ -20,10 +21,19 @@ const supabase = useSupabaseClient()
 const loading = ref(false)
 const email = ref('')
 const password = ref('')
+const username = ref('')
 
 const handleSignUp = async () => {
   loading.value = true
-  const { error } = await supabase.auth.signUp({ email: email.value, password: password.value })
+  const { error } = await supabase.auth.signUp({
+    email: email.value,
+    password: password.value,
+    options: {
+      data: {
+        display_name: username.value
+      },
+    },
+  })
   if (error) alert(error.message)
   loading.value = false
 }
