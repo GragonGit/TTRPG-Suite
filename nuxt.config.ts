7f4abd2bb19e4faa016ddc529401b0129fcbb324
Gragon <gragonator02@gmail.com>
2024-08-05 13:18:01 +0200

    Created initial landing page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "TTRPG Suite"
+      title: "TTRPG Suite",
+      bodyAttrs: {
+        class: 'baseLight accentPurple'
+      }
     }
   },
 
@@ -22,7 +25,7 @@ export default defineNuxtConfig({
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJnbWtraHZ4emduanp0b3Z5YnJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI3MjIxOTMsImV4cCI6MjAzODI5ODE5M30.H_N7pYlGk_uhxSa6LKqqRjVSLNUy0wwUqHQz7QDGJhg',
     redirectOptions: {
       login: '/auth/login',
-      callback: '/home',
+      callback: '',
       include: undefined,
       exclude: ['/', '/auth/signup'],
       cookieRedirect: false,
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,89 @@
 <template>
-  <div>
-    <h1>TTRPG Suite</h1>
+<section>
+  <div class="title">
+    <h1>Welcome to the <br><span>TTRPG Suite</span></h1>
+    <p>The tool covering every aspect of table top role playing. From characters to maps to playing together. Start your adventure now!</p>
   </div>
+  <div class="buttons">
+    <NuxtLink id="loginButton" to="/auth/login">Login</NuxtLink>
+    <NuxtLink id="signUpButton" to="/auth/signup">Sign Up</NuxtLink>
+  </div>
+</section>
 </template>
+
+<style lang="sass" scoped>
+  section
+    position: fixed
+    top: 0
+    bottom: 15rem
+    left: 0
+    right: 0
+    width: fit-content
+    height: fit-content
+    margin: auto
+
+    text-align: center
+
+  h1
+    font-size: 6rem
+    line-height: 6rem
+
+    margin: 0rem
+
+    span
+      background-clip: text
+      background-color: var(--accent-1)
+      background-image: linear-gradient(180deg, var(--accent-2) 0, var(--accent-1) 60%)
+      background-size: 100%
+      font-weight: 600
+      color: transparent
+
+  p
+    font-size: 1.25rem
+    font-weight: 500
+    line-height: 2rem
+
+    color: var(--text-2)
+
+    max-width: 40rem
+    margin: 0
+
+  .title
+    display: flex
+    flex-direction: column
+    align-items: center
+    gap: 1rem
+    margin-bottom: 4rem
+
+  .buttons
+    display: flex
+    justify-content: center
+    gap: 2rem
+
+  a
+    font-size: 1.25rem
+    text-decoration: none
+
+    border: solid 1px
+    border-radius: 0.75rem
+
+    padding: 0.75rem 1rem
+
+    transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out
+
+  #loginButton
+    border-color: var(--accent-1)
+    background-color: var(--accent-1)
+    color: var(--accent-1-text)
+
+    &:hover
+      background-color: var(--accent-1-hover)
+
+  #signUpButton
+    border-color: var(--accent-1)
+    color: var(--accent-1)
+
+    &:hover
+      background-color: var(--accent-1)
+      color: var(--accent-1-text)
+</style>
