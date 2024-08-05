2c2ec3dc64a59b23840a3add288e97492f1580a9
Gragon <gragonator02@gmail.com>
2024-08-06 00:54:59 +0200

    Added login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,30 +1,85 @@
 <template>
-<form @submit.prevent="handleLogin">
-  <div>
-    <h1>TTRPG Suite Log In</h1>
-    <div>
-      <input type="email" placeholder="Email" v-model="email" />
-      <input type="password" placeholder="Password" v-model="password" />
+<div class="login">
+  <h2>Log In</h2>
+  <form @submit.prevent="loginWithPassword()">
+    <div class="formLabelTextDiv">
+      <label class="formLabel" for="emailInput">Email</label>
+      <input id="emailInput" class="formTextField" type="text" name="emailInput" placeholder="Email" v-model="email" />
     </div>
-    <div>
-      <input type="submit" :value="loading ? 'Loading' : 'Send magic link'" :disabled="loading" />
+
+    <div class="formLabelTextDiv">
+      <label class="formLabel" for="passwordInput">Password</label>
+      <input id="passwordInput" class="formTextField" type="password" name="passwordInput" placeholder="Password"
+        v-model="password" />
     </div>
-  </div>
-</form>
-<NuxtLink to="/auth/signup">Sign up</NuxtLink>
+
+    <div class="loginSignup">
+      <input class="filledAccentButton" type="submit" :value="isLoading ? 'Loading' : 'Log In'" :disabled="isLoading" />
+      <NuxtLink class="signupLink" to="/auth/signup">Sign up instead</NuxtLink>
+    </div>
+
+    <NuxtLink to="/">Back to Start</NuxtLink>
+  </form>
+</div>
 </template>
 
 <script lang="ts" setup>
 const supabase = useSupabaseClient()
 
-const loading = ref(false)
 const email = ref('')
 const password = ref('')
+const isLoading = ref(false)
 
-const handleLogin = async () => {
-  loading.value = true
+const loginWithPassword = async () => {
+  isLoading.value = true
+
   const { error } = await supabase.auth.signInWithPassword({ email: email.value, password: password.value })
-  if (error) alert(error.message)
-  loading.value = false
+  if (error) {
+    password.value = ''
+    alert('Your email or password seem to be incorrect.')
+  } else {
+    return navigateTo('/home')
+  }
+
+  isLoading.value = false
 }
 </script>
+
+<style lang="sass" scoped>
+  .login
+    display: flex
+    justify-content: center
+    align-items: center
+    flex-direction: column
+
+    position: absolute
+    top: -15rem
+    bottom: 0
+    left: 0
+    right: 0
+  
+  form
+    display: flex
+    flex-direction: column
+    align-items: center
+
+    width: 32rem
+    gap: 2rem
+    margin: 4rem
+
+  .loginSignup
+    display: flex
+    flex-direction: column
+    align-items: center
+
+    width: 100%
+    margin-top: 0.75rem
+    gap: 1rem
+
+  input[type=submit]
+    width: 100%
+
+  .signupLink
+    color: var(--accent-1)
+
+</style>
