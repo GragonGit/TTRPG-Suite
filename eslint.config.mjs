e4b5ba8afe5f2085476b5e18c72528822d4e68bd
Gragon <gragonator02@gmail.com>
2024-08-10 17:03:57 +0200

    Set ESLint config rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,5 +2,11 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/first-attribute-linebreak': 'off'
+    }
+  }
 )
