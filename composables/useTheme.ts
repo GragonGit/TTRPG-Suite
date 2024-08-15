d011d9e55372773d53cde224484812456ff25bf1
Gragon <gragonator02@gmail.com>
2024-08-15 15:07:25 +0200

    Added accent color for dark mode

--- composables/useTheme.ts
+++ composables/useTheme.ts
@@ -6,15 +6,17 @@ export type AccentColor = 'accentPurple' | 'accentLightBlue'
 
 export const useTheme = () => {
   const initThemes = () => {
-    document.body.classList.remove('baseLight', 'accentPurple')
-    document.body.classList.add(baseTheme.value)
-    document.body.classList.add(accentColor.value)
+    document.body.classList.remove('baseLight', 'accentPurpleLight')
+    setBaseTheme(baseTheme.value as BaseTheme)
+    setAccentColor(accentColor.value as AccentColor)
   }
 
   const setBaseTheme = (theme: BaseTheme) => {
     document.body.classList.remove(baseTheme.value)
+    document.body.classList.remove(accentColor.value + (isLightBaseTheme() ? 'Light' : 'Dark'))
     baseTheme.value = theme
     document.body.classList.add(baseTheme.value)
+    document.body.classList.add(accentColor.value + (isLightBaseTheme() ? 'Light' : 'Dark'))
   }
 
   const isLightBaseTheme = () => baseTheme.value === 'baseLight'
@@ -22,9 +24,9 @@ export const useTheme = () => {
   const toggleBaseTheme = () => setBaseTheme(isLightBaseTheme() ? 'baseDark' : 'baseLight')
 
   const setAccentColor = (color: AccentColor) => {
-    document.body.classList.remove(accentColor.value)
+    document.body.classList.remove(accentColor.value + (isLightBaseTheme() ? 'Light' : 'Dark'))
     accentColor.value = color
-    document.body.classList.add(accentColor.value)
+    document.body.classList.add(accentColor.value + (isLightBaseTheme() ? 'Light' : 'Dark'))
   }
 
   return {
